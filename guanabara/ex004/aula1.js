c17636be2dfca36e3e5d3cb40139f84146c7a5bc
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-07-18 14:19:18 -0300

    finalizando o modulo dos operadores

---
+++ guanabara/ex004/aula1.js
@@ -0,0 +1,4 @@
+// modulos
+// 5 % 2 = 1; 
+// exponenciação
+// 5 ** 2; ou 5 **= 2 isso é um operador de autoatribuição
---
+++ guanabara/ex004/aula2.js
@@ -0,0 +1,28 @@
+/* operadores relacionais:
+<,>,==,=!,>=,<=
+
+IDENTIDADE
+5 == '5' = true
+5==='5' = false
+*/
+
+/*operadores lógicos
+! = negação
+ && = conjunção num1 e num2 = dois valores lógicos(num1 pode ser falso e num2 pode ser verdadeiro mas os dois tem q dar o true)
+|| = num1 ou num2 (esse, se um for verdadeiro, o resutado vai ser)
+var a = 5;
+
+var b = 8;
+primeiro os operadores aritiméticos depois os relacionais e depois os logicos
+a > 8 && b % 2 == 0
+
+
+TERNARIOS
+teste?true:false
+
+var media = 5.5;
+media > 7 ?'aprovado':'reprovado'
+
+var x = 8;
+var res =  x % 2 == 0?5:9;
+*/

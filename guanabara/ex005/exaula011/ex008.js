be1f4c7946bf132e2089e51d272036ae86f0909e
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-07-26 19:56:05 -0300

    ex009

---
+++ guanabara/ex005/exaula011/ex008.js
@@ -0,0 +1,13 @@
+console.log('o console funcionou corretamente')
+
+var vel = 50.5
+
+console.log(`a velocidade do seu carro é ${vel} Km/h`)
+
+// condição simples
+
+if(vel > 60){
+    console.log('ultrapassou hein')
+}
+
+console.log('dirija sempre usando cinto de segurança')

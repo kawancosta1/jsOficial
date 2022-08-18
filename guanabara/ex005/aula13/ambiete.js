c6a697688707208d9b5a725f9b54804a23cfba91
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-08-18 19:15:42 -0300

    opa

---
+++ guanabara/ex005/aula13/ambiete.js
@@ -0,0 +1,26 @@
+// var c = 1;
+//estrtutura de repetição com teste logico no inicio
+// while(c < 6){
+//     console.log('tudo bem?')
+    
+//     c++
+
+// }
+var c = 1;
+do{
+    console.timeLog('oi')
+}while(c<=6)
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/

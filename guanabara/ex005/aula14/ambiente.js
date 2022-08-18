518718bfe568d282368c4cd275e154a0243700f9
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-08-18 19:24:07 -0300

    for

---
+++ guanabara/ex005/aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1;
+while(c<=5){
+    console.log(c)
+    c++
+    
+}
+*/
+console.log('vai começar....')
+
+//inicialização;condição;incremento
+for(var c = 1; c<=10; c++){
+    console.log(c)
+}
+console.log('fim')

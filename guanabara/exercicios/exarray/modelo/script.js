13829adc951402bc8d3bb2deaeeffcf74920f30c
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-09-27 06:13:40 -0300

    terminando

--- guanabara/exercicios/exarray/modelo/script.js
+++ guanabara/exercicios/exarray/modelo/script.js
@@ -5,21 +5,51 @@ let menor = document.querySelector('#menor')
 let maior = document.querySelector('#maior')
 let media =  document.querySelector('#media')
 let soma =  document.querySelector('#soma')
+ function isNumero(n){
 
+    if(Number(n) >= 1 && Number(n) <= 100){
+return true
+    }else{
+        return false
+    }
+
+ }
+
+ function Inlista(n, array){
+//o -1 indica que o valor não foi encontrado na lista
+if(array.indexOf(Number(n)) != -1){
+    return true
+}else{
+//agora se ele for encontrado 
+return false
+}
+
+ }
 
 // essa is numero e in lista eu vou criar essas funções ainda
-function adicionaSelect(){
-    if(isNumero(fnum.value) && !Inlista(num.value)){
+function adicionas(){
+    //o push é pra add valores
+   
+    //function isNumero()
+    if(isNumero(fnum.value) && !Inlista(fnum.value, num )){
+        num.push(Number(fnum.value))
 item = document.createElement('option')
 // aqui eu to add numeros digitados no array, por isso passei pra number
-num = Number(fnum.value)
+
 item.text = `você adicionou ${fnum.value}`
 
 
 tab.appendChild(item)
-    } alert('valor invalido ou já encontrado na lista')
+    }else{ alert('valor invalido ou já encontrado na lista')
+}
+fnum.value = ''
+fnum.focus()
+
+// pra focar na text box lá
+
 
 }
+
 
 function finalizar(){
     if(num == '' || num.lengh == 0){

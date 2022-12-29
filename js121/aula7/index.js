e55e167cdd2e46aeeda8055ab56b40c5327e9d2c
kawan <kawancosta063@gmail.com>
2022-12-29 20:17:23 -0300

    aula 8ex

--- js121/aula7/index.js
+++ js121/aula7/index.js
@@ -3,3 +3,22 @@
 
 const nome = 'joao'
 console.log(nome)
+//string = text | number = numero
+
+// + - *
+const firstNumber = 5; //number
+const secondNumber = 10;
+const conta = firstNumber * secondNumber;
+const duplicada = conta * 2
+let triplicado = conta * 3;
+console.log(conta)
+console.log(duplicada)
+
+//podemos evoluir o valor ao invés de mudar eles com a const
+
+// o js tem tipagem dinamica ou seja ele identifica o tipo de variavel, podemos ver assim:
+console.log(typeof firstNumber);//number
+console.log(typeof nome)//string
+
+//o sinal de soma é usado para concatenar e fazer somas bem maneirinhas 
+

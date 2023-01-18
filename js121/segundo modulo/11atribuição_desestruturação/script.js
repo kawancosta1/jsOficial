c595ee2884c09506c06d295b3e8e158b3a3e1bc4
kawan <98924310+kawancosta1@users.noreply.github.com>
2023-01-18 17:15:53 -0300

    atribuição

---
+++ js121/segundo modulo/11atribuição_desestruturação/script.js
@@ -0,0 +1,33 @@
+//atribuição via desestruturação, ou seja, utilizar arrays para atribuir a uma var ou let
+
+let a ="A"
+let b ="B"
+let c ="C"
+
+// [a,b,c] = [b,c,a]
+
+//metodo antigo
+// const numeros = [1,2,3,4,5,6,7,8]
+// const primeiro = numeros[0]
+
+const numeros = [1,2,3,4,5,6,7,8]
+//ai ó nem precisa colocar o indice é só colocar na ordem as variáveis que você quer declarar valores cujo o array possui
+const [primeiroNumero,segundoNumero] = numeros;
+
+//rest = ...
+const numeros2 = [1000,2000,3000,4000,5000]
+
+//ele atribui na ordem do array, iniciando do 0
+const [um,dois,tres,...resto] = numeros2;
+console.log(um,dois,tres,resto)
+
+const numero3 = [
+//      0       1      2
+//   0 1 2   0 1 2  0 1 2
+    [1,2,3] [4,5,6] [7,8,9]
+]
+
+//a virgula serve para pular um indice
+const [,dois1,tres1] = numero3
+
+console.log(numero3[1][2])

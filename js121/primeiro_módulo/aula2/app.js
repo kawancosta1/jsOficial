4752f41785c4d369d45261fb415c6c3b95490f3d
kawan <98924310+kawancosta1@users.noreply.github.com>
2024-03-25 13:25:02 -0300

    Update app.js

--- js121/primeiro_módulo/aula2/app.js
+++ js121/primeiro_módulo/aula2/app.js
@@ -1,3 +1,5 @@
+//  para exibir textos, usa aspas simples ou duplas
+// cada vez que usar a console. log, ela quebra a linha
 console.log("ola");
 function ola(opa){
    var result = opa * 2
@@ -5,4 +7,12 @@ function ola(opa){
     
 
 }
+
 console.log(ola(8))
+
+let brabo = "delícia"
+// isso é uma template string, ou seja, exibe texto e variáveis
+console.log(`ola ${brabo}`)
+
+// não usar vírgula pra decimal, mas sim ponto
+console.log(15.85)
